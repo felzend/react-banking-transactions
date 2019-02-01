9dc6e8f7d55292fb19cf32a5149a32b435527317
Felipe S. Fernandes <feleast.49@gmail.com>
2019-02-01 17:06:12 -0300

    API - Web Socket recebendo e enviando mensagens

--- api/BankTransactions/BankTransactions/Startup.cs
+++ api/BankTransactions/BankTransactions/Startup.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using BankTransactions.Database;
+using BankTransactions.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -49,9 +54,44 @@ namespace BankTransactions
                 app.UseHsts();
             }
 
+            app.UseCors("AllowLocalhost");
             app.UseHttpsRedirection();
             app.UseMvc();
-            app.UseCors("AllowLocalhost");
+
+            app.UseWebSockets();
+            app.UseMiddleware<WebSocketMiddleware>();
+            /*app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        //await WebSocketMiddleware.AddSocket(webSocket);
+                        await WebSocketMiddleware.Echo(webSocket);                        
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });*/
         }
+        public static async Task SendMessageAsync(WebSocket socket, string message)
+        {
+            if (socket.State != WebSocketState.Open)
+                return;
+
+            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message), offset: 0, count: message.Length),
+            messageType: WebSocketMessageType.Text,
+            endOfMessage: true,
+            cancellationToken: CancellationToken.None);
+        }
+
     }
 }
